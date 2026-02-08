e6ee1fd75b651fafc3d2f9116f3209ca37e18bad
Michelle Torres <anamich.2008@gmail.com>
2026-02-08 07:34:06 -0500

    Collect static files and fix WhiteNoise

--- config/settings.py
+++ config/settings.py
@@ -82,12 +82,13 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/alimentos/"
 LOGOUT_REDIRECT_URL = "/login/"
 
 CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com"]
